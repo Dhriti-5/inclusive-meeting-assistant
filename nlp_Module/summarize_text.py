3d81d075910eb72b890b09a0c6d2599f098bd450
Dhriti-5 <gandhi.dhriti2005@gmail.com>
2025-07-27 20:31:43 +0530

    change code to function style

--- nlp_Module/summarize_text.py
+++ nlp_Module/summarize_text.py
@@ -2,12 +2,6 @@ from transformers import pipeline
 
 summarizer = pipeline("summarization",model="sshleifer/distilbart-cnn-12-6")
 
-with open(r"C:\Users\Pc\Deep Learning Specialization\inclusive-meeting-assistant\output\transcript.txt", "r", encoding="utf-8") as f:
-    transcript = f.read()
-
-summary = summarizer(transcript, max_length=150, min_length=40, do_sample=False)
-
-print("\n--- Summary ---")
-print(summary[0]['summary_text'])
-
-
+def summarize_text(transcript):
+    summary = summarizer(transcript, max_length=150, min_length=40, do_sample=False)
+    return summary[0]['summary_text']
